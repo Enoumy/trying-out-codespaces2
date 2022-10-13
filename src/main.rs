efa01926aeb0aa8928c37dde6ab2c5c89f157432
Enoumy <enoumy@gmail.com>
2022-10-12 22:03:40 -0400

    z

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-#[derive(Debug)]
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
+#[derive(Debug, Hash, Eq, PartialEq, Ord, PartialOrd, Copy, Clone)]
 struct Point {
     x: i32,
     y: i32,
 }
 
-fn line2d(Point { x: x0, y: y0 }: Point, Point { x: x1, y: y1 }: Point) -> Vec<Point> {
+fn line_2d(Point { x: x0, y: y0 }: Point, Point { x: x1, y: y1 }: Point) -> Vec<Point> {
     let dx = (x1 - x0).abs();
     let sx = if x0 < x1 { 1 } else { -1 };
     let dy = -(y1 - y0).abs();
@@ -30,10 +33,62 @@ fn line2d(Point { x: x0, y: y0 }: Point, Point { x: x1, y: y1 }: Point) -> Vec<P
     }
 }
 
-fn main() {
-    let start = Point { x: 1, y: 2 };
-    let end = Point { x: 10, y: 5 };
+fn print_points(points: Vec<Point>) {
+    let point_set: HashSet<Point> = HashSet::from_iter(points);
+    let helper = |start: &Point, end: &Point| {
+        for y in start.y..=end.y {
+            for x in start.x..=end.x {
+                if point_set.contains(&Point { x, y }) {
+                    print!("x");
+                } else {
+                    print!(".");
+                }
+            }
+            println!("");
+        }
+    };
+    match point_set
+        .iter()
+        .fold(None, |acc, Point { x: xval, y: yval }| match acc {
+            None => {
+                let point = Point { x: *xval, y: *yval };
+                Some((point, point))
+            }
+            Some((Point { x: xmin, y: ymin }, Point { x: xmax, y: ymax })) => {
+                // This is sad. I think this can be avoided by implementing a custom comparator
+                // function, but I'd be even sadder.
+                let xmin = std::cmp::min(xmin, *xval);
+                let ymin = std::cmp::min(ymin, *yval);
+                let xmax = std::cmp::max(xmax, *xval);
+                let ymax = std::cmp::max(ymax, *yval);
+                let min = Point { x: xmin, y: ymin };
+                let max = Point { x: xmax, y: ymax };
+                Some((min, max))
+            }
+        }) {
+        None => (),
+        Some((min, max)) => helper(&min, &max),
+    }
+}
 
-    let line = line2d(start, end);
-    println!("{:?}", line);
+fn main() {
+    let width = 23;
+    let height = 5;
+    let test = |a, b| {
+        let (x0, y0, x1, y1) = match (a, b) {
+            (true, true) => (1, 1, width, height),
+            (false, false) => (width, height, 1, 1),
+            (false, true) => (1, height, width, 1),
+            (true, false) => (width, 1, 1, height),
+        };
+        let start = Point { x: x0, y: y0 };
+        let end = Point { x: x1, y: y1 };
+        let line = line_2d(start, end);
+        print_points(line);
+    };
+
+    test(false, false);
+    test(true, false);
+    test(true, true);
+    test(false, true);
 }
